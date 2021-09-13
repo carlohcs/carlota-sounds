43a198befd054d9b15f516deecd5148469db1bcf
carlohcs <carlohcs@gmail.com>
2021-09-12 21:41:01 -0300

    Release 1.2.0! - Nextjs updated to 11.1.1; - Better score on lighthouse; - Added html lang property;

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <>
             <title>Carlota Sounds - A reflection of sounds.</title>
